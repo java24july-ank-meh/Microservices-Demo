af5ce5e9382c13ab238758f92c8cf0f1ca60987b
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-10-22 14:21:48 -0400

    Added initial spring project

---
+++ src/main/java/com/revature/Microservice1DemoApplication.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1DemoApplication.class, args);
+	}
+}
