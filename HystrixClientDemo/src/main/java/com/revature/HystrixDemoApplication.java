5fc1d588a9c37cb9f381ec19f8521f5becd662d7
jackson.demaree <jackson.demaree@tamu.edu>
2017-10-23 11:44:51 -0400

    HystrixDashboard application

--- HystrixClientDemo/src/main/java/com/revature/HystrixDemoApplication.java
+++ HystrixClientDemo/src/main/java/com/revature/HystrixDemoApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.revature.service.HelloService;
 
 @EnableCircuitBreaker
+@EnableHystrix
 @SpringBootApplication
 @RestController
 public class HystrixDemoApplication {
